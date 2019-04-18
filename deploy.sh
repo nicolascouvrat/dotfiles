9362effa78b2c6be263ce34d65dc0a31f1b7c518
Nicolas Couvrat <nicolas.couvrat@datadoghq.com>
2019-04-17 23:42:49 -0400

    add more dotfiles, rework script

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,89 @@
 #!/bin/bash
+function_exists() {
+  # declare prints the code of then given function, silence it
+  declare -f $1 > /dev/null
+  # it returns 1 (failure) if nothing or 0 if found
+  echo $?
+}
 
-VIM_FOLDER=~/.vim
-BACKUP_FOLDER=~/vim_backup
+pick_logger() {
+  if [ $(function_exists log) -eq 0 ]; then
+    echo log
+  else
+    echo echo
+  fi
+}
 
-# prepare a clean vim folder
-if [ -d $VIM_FOLDER ]; then
-    echo Found existing $VIM_FOLDER, moving it to $BACKUP_FOLDER...
-    # if the backup folder already exists, clean it up
-    [ -d $BACKUP_FOLDER ] && (echo WARNING: removing old backup && rm -rf $BACKUP_FOLDER)
-    mv $VIM_FOLDER $BACKUP_FOLDER
-fi
+simple_link() {
+  # usage simple_link file_name target_path
+  # must be used in the directory in which simple_link is called
+  local log=$(pick_logger)
+  local current=$(pwd)
+  local target=$3$2
+  if [ -f $target ]; then
+    $log warn "Found file $target. Removing..."
+    rm $target
+  fi
+  $log info "Linking $1 to $target"
+  ln -s $current/$1 $target
+}
 
-echo Creating fresh $VIM_FOLDER...
-mkdir $VIM_FOLDER
+directory_link() {
+  # usage: folder_link target_dir
+  # recursively simlinks the entire directory
+  local log=$(pick_logger)
+  local target=$1
+  local backup=$1_backup
+  local base=$(pwd)
+  # prepare a clean state
+  if [ -d $target ]; then
+    $log warn "Found existing folder $target, moving it to $backup..."
+    [ -d $backup ] && ($log warn "Removing old backup!" && rm -rf $backup)
+    mv $target $backup
+  fi
+  mkdir $target
+  recursive_link $(pwd) $target
+}
+
+recursive_link() {
+  # usage: recursive_link root_dir target_root_dir
+  # must be used first in root_dir
+  local log=$(pick_logger)
+  for d in *; do
+    if [ -d $d ]; then
+      mkdir $2/$d
+      (cd $d && recursive_link $1/$d $2/$d)
+    else
+      simple_link $d $2/$d
+    fi
+  done
+}
+
+ROOT=$(pwd)
+
+### BASH ###
+cd ./bash
+simple_link bashrc ~/.bashrc
+source ~/.bashrc # get the better logging func
+simple_link bash_profile ~/.bash_profile
+cd bash
+directory_link ~/.bash
+log info "Installing gitawareprompt..."
+git clone https://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
+# reload again
+source ~/.bashrc
+cd $ROOT
+
+### INPUTRC ###
+simple_link inputrc ~/.inputrc
 
 ### VIM ###
 cd ./vim
-base_dir=$(pwd)
+directory_link ~/.vim
 
-# go through the vim directory recursively,
-# and creates appropriate simlinks in the ~/.vim folder
-recursive_linking() {
-    for d in *; do
-        local current_dir=$(pwd)
-        local relative_dir=${current_dir#$base_dir}
-        if [ -d "$d" ]; then
-            # if dir, the create folder (otherwise linking will fail)
-            mkdir $VIM_FOLDER$relative_dir/$d
-            (cd $d && recursive_linking)
-        else
-            local link_target=$relative_dir/$d
-            echo Linking $d to $VIM_FOLDER$link_target
-            ln -s $current_dir/$d $VIM_FOLDER$link_target
-        fi
-    done
-}
-
-recursive_linking
 # Install vundle
-echo Installing Vundle..
+log info "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo All set!
+cd $ROOT
 
-### VIM DONE ###
+log info "All set!"
