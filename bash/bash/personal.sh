88e4a3302b88df46e7fbe20ab91a2fa2da7e18cb
Nicolas Couvrat <nicolas.d.couvrat@gmail.com>
2020-03-18 15:13:03 +0900

    auto start ssh agent

--- bash/bash/personal.sh
+++ bash/bash/personal.sh
@@ -1,1 +1,9 @@
 export PATH=$PATH:/usr/local/go/bin
+
+# Auto add ssh key and do it only once after each reboot
+if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+  eval `ssh agent`
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l > /dev/null || ssh-add
