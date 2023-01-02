21af6ce69ea0e5b4823c239cff577c183f0f23ec
Nicolas Couvrat <nicolas.couvrat@datadoghq.com>
2023-01-02 16:44:40 +0100

    Improve reusability

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,10 @@ function_exists() {
 pick_logger() {
   if [ $(function_exists log) -eq 0 ]; then
     echo log
+    return
   else
     echo echo
+    return
   fi
 }
 
@@ -31,18 +33,22 @@ simple_link() {
 directory_link() {
   # usage: folder_link target_dir
   # recursively simlinks the entire directory
+  local target=$1
+  backup_and_clean $target
+  mkdir $target
+  recursive_link $(pwd) $target
+}
+
+backup_and_clean() {
   local log=$(pick_logger)
   local target=$1
   local backup=$1_backup
-  local base=$(pwd)
   # prepare a clean state
   if [ -d $target ]; then
     $log warn "Found existing folder $target, moving it to $backup..."
     [ -d $backup ] && ($log warn "Removing old backup!" && rm -rf $backup)
     mv $target $backup
   fi
-  mkdir $target
-  recursive_link $(pwd) $target
 }
 
 recursive_link() {
@@ -60,22 +66,28 @@ recursive_link() {
 }
 
 ROOT=$(pwd)
+# get the logging function
+source ./mini_zshrc
 
 ### BASH ###
 cd ./bash
 simple_link bashrc ~/.bashrc
-source ~/.bashrc # get the better logging func
 simple_link bash_profile ~/.bash_profile
 cd bash
 directory_link ~/.bash
 log info "Installing gitawareprompt..."
 git clone https://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
 # reload again
-source ~/.bashrc
+cd $ROOT
+
+### ZSH ###
+cd ./zsh
+simple_link zshrc ~/.zshrc
 cd $ROOT
 
 ### INPUTRC ###
 simple_link inputrc ~/.inputrc
+simple_link tmux.conf ~/.tmux.conf
 
 ### VIM ###
 cd ./vim
@@ -86,4 +98,31 @@ log info "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd $ROOT
 
-log info "All set!"
+log info "Installing Jenv..."
+backup_and_clean ~/.jenv
+git clone https://github.com/jenv/jenv.git ~/.jenv
+cd $ROOT
+log info "Configuring Jenv..."
+jenv enable-plugin export
+jenv enable-plugin maven
+
+log info "Installing Pyenv..."
+backup_and_clean ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd $ROOT
+
+log info "Installing tmux..."
+brew install tmux
+
+# To make pbcopy work from tmux
+log info "Installing reattach-to-user-namespace..."
+brew install reattach-to-user-namespace
+
+
+log info "Installing Dracula Terminal.app theme..."
+backup_and_clean ~/dracula-terminal-theme
+git clone https://github.com/dracula/terminal-app.git ~/dracula-terminal-theme
+log info "To finish setting up terminal colors, go to Terminal > Settings, click gear icon then import Dracula.terminal from $HOME/dracula-terminal-theme"
+cd $ROOT
+
+log info "All set! Restart your terminal now :)"
